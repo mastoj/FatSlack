6adf8f7fe09c99bca3505c165e03f74c4bd55426
Tomas Jansson <tomas.jansson@unacast.com>
2017-11-07 23:43:09 +0100

    Bump version to 0.0.3-beta

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@ if test "$OS" = "Windows_NT"
 then
   # use .Net
 
+  .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
   .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
@@ -12,6 +18,13 @@ then
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else
   # use mono
+  # use mono
+  mono .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
   mono .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
