8560d77416c26c69c3ac7d27120ce29a7ca48d0e
Tomas Jansson <tomas.jansson@unacast.com>
2018-03-06 23:44:25 +0100

    Bump version to 0.0.11-beta

--- src/FatSlack.Bot/AssemblyInfo.fs
+++ src/FatSlack.Bot/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FatSlack.Bot")>]
 [<assembly: AssemblyProductAttribute("FatSlack")>]
 [<assembly: AssemblyDescriptionAttribute("Slack bot/app framework in F# for F#")>]
-[<assembly: AssemblyVersionAttribute("0.0.10")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.10")>]
+[<assembly: AssemblyVersionAttribute("0.0.11")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.11")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FatSlack.Bot"
     let [<Literal>] AssemblyProduct = "FatSlack"
     let [<Literal>] AssemblyDescription = "Slack bot/app framework in F# for F#"
-    let [<Literal>] AssemblyVersion = "0.0.10"
-    let [<Literal>] AssemblyFileVersion = "0.0.10"
+    let [<Literal>] AssemblyVersion = "0.0.11"
+    let [<Literal>] AssemblyFileVersion = "0.0.11"
     let [<Literal>] AssemblyConfiguration = "Release"
