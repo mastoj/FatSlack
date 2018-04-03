6c8e4270a0ac54c04e9067d4f8b2b29003f4370d
Tomas Jansson <tomas.jansson@unacast.com>
2018-04-03 12:26:44 +0200

    Delete old pod on deploy

--- sample/FatSlack.Sample/scripts/run-pod.sh
+++ sample/FatSlack.Sample/scripts/run-pod.sh
@@ -2,4 +2,5 @@
 
 _gitsha=$(git rev-parse --short HEAD)
 _slacktoken=$1
+kubectl delete po fatslacksample
 kubectl run fatslacksample --port=8080 --restart=Never --image=mastoj/fatslack.sample:$_gitsha -- $_slacktoken
